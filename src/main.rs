b1228aa66880e4fa32eb8a6877bc52a3cedb9864
Patrick Buller <pbuller@deerplain.com>
2023-12-06 07:26:02 -0600

    Add cl argument for omitting newline

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
-use clap::App;
+use clap::{Arg,  App};
 
 fn main() {
     let _matches = App::new("echo")
         .version("0.1.0")
         .author("Patrick Buller")
         .about("Rust based version of echo")
+        .arg(
+            Arg::with_name("omit newline")
+                .short("n")
+                .help("Do not print with new line")
+                .takes_value(false),
+        )
         .get_matches();
 
     
