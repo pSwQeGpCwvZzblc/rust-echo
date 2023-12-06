5db181d4a385606cfb303a9e0ffaae3f1a20cf68
Patrick Buller <pbuller@deerplain.com>
2023-12-06 07:18:56 -0600

    Base for using clap library

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use clap::App;
+
 fn main() {
+    let _matches = App::new("echo")
+        .version("0.1.0")
+        .author("Patrick Buller")
+        .about("Rust based version of echo")
+        .get_matches();
+
+    
     println!("{:?}", std::env::args());
 }
